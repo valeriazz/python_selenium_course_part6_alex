9855b9747748d4c8971b056b81c3f24d23307a14
valeriazz <valeriazlk21@gmail.com>
2022-09-22 15:42:29 +0300

    код по Pytest

---
+++ tests/test_order.py
@@ -0,0 +1,33 @@
+"""для установки очередности выполнения тестов используется библиотека pytest-ordering,
+установка через терминал: pip3 install pytest-ordering"""
+import pytest
+
+
+@pytest.mark.run(order=2) # установка порядка для запуска очередности тестов
+def test_method_1(set_up):
+    print('Метод 1')
+
+
+@pytest.mark.run(order=1)
+def test_method_2(set_up):
+    print('Метод 2')
+
+
+@pytest.mark.run(order=6)
+def test_method_3(set_up):
+    print('Метод 3')
+
+
+@pytest.mark.run(order=4)
+def test_method_4(set_up):
+    print('Метод 4')
+
+
+@pytest.mark.run(order=3)
+def test_method_5(set_up):
+    print('Метод 5')
+
+
+@pytest.mark.run(order=5)
+def test_method_6(set_up):
+    print('Метод 6')
